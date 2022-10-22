1974d8a18f83f540e79bf2dc91ccb573db89223b
NNieto25 <00199019@uca.edu.sv>
2022-10-21 23:43:41 -0600

    Added test for front end

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,32 @@
-import { describe } from 'vitest'
+import { render, screen } from '@testing-library/react';
+import { describe } from 'vitest';
+import App from './App';
 
-describe('App', () => { 
-  it.todo('should render');
-  it.todo('should have title "Mi DUI es valido?"');
-  it.todo('should have an input text');
-  it.todo('should have a button with text "Validar"');
-  /* Lo que demas que se les ocurra que puedan probar */
-})
+describe('App', () => {
+  // it.todo('should render');
+  it('App rendering', () => {
+    render(<App/>)
+    const AppElement = screen.getByTestId('index');
+    expect(AppElement).toBeInTheDocument();
+  });
+
+  //it.todo('should have title "Mi DUI es valido?"');
+  it('Correct Title', () => {
+    render(<App/>)
+    expect(screen.getByText(`Mi DUI es valido?`)).toBeInTheDocument();
+  });
+
+  //it.todo('should have an input text');
+  it('Input is rendering', () => {
+    render(<App/>)
+    const inputElement = screen.getByTestId('input');
+    expect(inputElement).toBeInTheDocument();
+  });
+
+  //it.todo('should have a button with text "Validar"');
+  it('"validar" button is rendering', () => {
+    render(<App/>)
+    const buttonElement = screen.getByRole('button', {name: 'Validar'});
+    expect(buttonElement).toBeInTheDocument();
+  });
+});
--- src/App.tsx
+++ src/App.tsx
@@ -6,15 +6,15 @@ function App() {
   const [value, setValue] = useState("")
 
   const onSubmit = () => { 
-    if (duiValidator(value)) {
-      console.log('es true');
-    } else { 
-      console.log('no es true');
-    }
+    const message = duiValidator(value)
+    ? "El dui es valido"
+    : "El dui NO es valido";
+    
+    alert(message);
   }
 
   return (
-    <div className='relative bg-slate-300 w-full min-h-screen flex items-center justify-center flex-col'>
+    <div className='relative bg-slate-300 w-full min-h-screen flex items-center justify-center flex-col' data-testid="index">
       <div className='w-full max-w-lg text-center flex flex-col gap-16'>
         <h1 className='text-5xl font-bold'>Mi DUI es valido?</h1>
         <form
@@ -25,6 +25,7 @@ function App() {
            } }
         >
           <input
+            data-testid="input"
             placeholder='000000000'
             className='rounded p-2 w-full'
             value={value}
